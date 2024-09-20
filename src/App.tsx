8d87594b12f880accd161a2248a82c1947f237d7
rifkifajarramadhani <rifkid56@gmail.com>
2024-09-20 23:25:55 +0700

    chore: update and re-arrange with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { Footer } from "./components/Footer"
-import { Header } from "./components/Header"
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <>
-      <Header />
-
-      <main>
-      </main>
-
-      <Footer />
+      <HomePage />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+
+      <main></main>
+
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
